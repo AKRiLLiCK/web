04bfb106e16db08d8e29cedbc418c91bd6cc66fe
AKRiLLiCK <akrillick@proton.me>
2026-04-22 10:44:42 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
+});
