33775a94bdc84aad3ed69a4d52f9d08e330aff26
AKRiLLiCK <akrillick@proton.me>
2026-04-22 11:34:25 +0200

    6

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://AKRiLLiCK.github.io', 
-  base: '/web', 
+  site: 'https://akrillick.github.io', 
+  base: '/web',
   integrations: [mdx()],
   trailingSlash: 'always',
 });
